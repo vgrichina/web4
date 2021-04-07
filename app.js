289244cb9585f2af47636311d93f91b7d9a11c71
Vladimir Grichina <vgrichina@gmail.com>
2021-04-07 02:59:22 -0700

    Add rough design of other routes

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const app = new Koa();
 const Router = require('koa-router');
 const router = new Router();
 
-
-router.get('/web4/:contractId/:methodName', withNear, async ctx => {
+router.get('/web4/contract/:contractId/:methodName', withNear, async ctx => {
     const {
         params: { contractId, methodName },
         query,
@@ -35,6 +34,36 @@ router.get('/web4/:contractId/:methodName', withNear, async ctx => {
 
     ctx.body = await account.viewFunction(contractId, methodName, methodParams);
 });
+
+router.get('/web4/login');
+// TODO: Generate private key, return in cookie and redirect to wallet for login
+
+router.post('/web4/logout');
+// TODO: Remove private key cookie (ideally also somehow remove key from account?)
+
+router.post('/web4/contract/:contractId/:methodName', withNear, async ctx => {
+    // TODO: Sign transaction with private key and account from cookies
+
+    // TODO: Accept both json and form submission
+});
+
+// TODO: Do contract method call according to mapping returned by web4_routes contract method
+// TODO: Use web4_get method in smart contract as catch all if no mapping?
+// TODO: Or is mapping enough?
+router.get('/(.*)', ctx => {
+    // TODO: Allow returning different types of content, including references to external stuff like IPFS
+    ctx.body = ctx.path;
+});
+
+// TODO: submit transaction mapping path to method name
+router.post('/(.*)', ctx => {
+    ctx.body = ctx.path;
+});
+
+
+// TODO: Need to query smart contract for rewrites config
+
+
 
 app
     .use(async (ctx, next) => {
