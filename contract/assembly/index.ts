2d20afb8c315d6c5c0d2d90b4ec4a644f54a3135
Vladimir Grichina <vgrichina@gmail.com>
2021-04-21 23:16:54 -0700

    Prototype simple HTML builder

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -3,6 +3,7 @@ import { context, ContractPromise, ContractPromiseBatch, logging, storage, u128,
 
 @nearBindgen
 class Web4Request {
+    accountId: string | null;
     path: string;
     params: Map<string, string>;
     query: Map<string, Array<string>>;
@@ -14,18 +15,76 @@ class Web4Response {
     body: Uint8Array;
 }
 
+class HtmlAttributes {
+    id: string | null;
+    name: string | null;
+    class: string | null;
+    style: string | null;
+
+    toString(): string {
+        let result = "";
+        if (this.id) {
+            result += "id=";
+            result += this.id!;
+        }
+        if (this.name) {
+            result += "name=";
+            result += this.name!;
+        }
+        if (this.class) {
+            result += "class=";
+            result += this.class!;
+        }
+        if (this.style) {
+            result += "style=";
+            result += this.style!;
+        }
+        return result;
+    }
+}
+
+class HtmlFormAttributes extends HtmlAttributes {
+    action: string | null;
+    method: string = "POST";
+
+    toString(): string {
+        let result = super.toString();
+        if (this.action) {
+            result += "action=";
+            result += this.action!;
+        }
+        if (this.method) {
+            result += "method=";
+            result += this.method;
+        }
+        return result;
+    }
+}
+
+function form(attrs: HtmlFormAttributes, content: string[] | null = null): string {
+    return '<form ' + attrs.toString() + '>' + (content ? content.join('\n') : '') + '</form>';
+}
+
+function textarea(attrs: HtmlAttributes, content: string[] | null = null): string {
+    return '<textarea ' + attrs.toString() + '>' + (content ? content.join('\n') : '') + '</textarea>';
+}
+
+function button(attrs: HtmlAttributes, content: string[] | null = null): string {
+    return '<button ' + attrs.toString() + '>' + (content ? content.join('\n') : '') + '</button>';
+}
+
 function htmlResponse(text: string): Web4Response {
     return { contentType: 'text/html; charset=UTF-8', body: util.stringToBytes(text) };
 }
 
 export function web4_get(request: Web4Request): Web4Response {
     if (request.path == '/test') {
-        return htmlResponse(`
-            <form action="/web4/contract/guest-book.testnet/addMessage" method="post">
-                <textarea name="text"></textarea>
-                <button name="submit">Post</button>
-            </form>
-        `);
+        return htmlResponse(form({ action: "/web4/contract/guest-book.testnet/addMessage" }, [
+            textarea({ name: "text" }),
+            button({ name: "submit" }, ["Post"])
+        ]));
+    }
+
     if (request.accountId) {
         return htmlResponse('Hello to <b>' +  request.accountId! + '</b> from <code>' + request.path + '</code>');
     }
