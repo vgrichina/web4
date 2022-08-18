fc35f904ddfdc03e3aec6b285323cd7f78c6e164
Vladimir Grichina <vgrichina@gmail.com>
2022-08-18 00:38:07 -0700

    Style guest book UI page

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -19,31 +19,29 @@ class Web4Response {
     preloadUrls: string[] = [];
 }
 
-// TODO: Quote attributes
+function quotedAttribute(name: string, value: string): string {
+    return `${name}="${value.replaceAll('"', '&quot;')}"`;
+}
 
 class HtmlAttributes {
     id: string | null;
     name: string | null;
-    class: string | null;
+    className: string | null;
     style: string | null;
 
     toString(): string {
         let result = "";
         if (this.id) {
-            result += "id=";
-            result += this.id!;
+            result += quotedAttribute("id", this.id!);
         }
         if (this.name) {
-            result += "name=";
-            result += this.name!;
+            result += quotedAttribute("name", this.name!);
         }
-        if (this.class) {
-            result += "class=";
-            result += this.class!;
+        if (this.className) {
+            result += quotedAttribute("class", this.className!);
         }
         if (this.style) {
-            result += "style=";
-            result += this.style!;
+            result += quotedAttribute("style", this.style!);
         }
         return result;
     }
@@ -56,27 +54,29 @@ class HtmlFormAttributes extends HtmlAttributes {
     toString(): string {
         let result = super.toString();
         if (this.action) {
-            result += " action=";
-            result += this.action!;
+            result += quotedAttribute("action", this.action!);
         }
         if (this.method) {
-            result += " method=";
-            result += this.method;
+            result += quotedAttribute("method", this.method);
         }
         return result;
     }
 }
 
+function htmlTag(tagName: string, attrs: HtmlAttributes, content: string[] | null = null): string {
+    return `<${tagName} ${attrs}>${content ? content.join('\n') : ''}</${tagName}>`;
+}
+
 function form(attrs: HtmlFormAttributes, content: string[] | null = null): string {
-    return '<form ' + attrs.toString() + '>' + (content ? content.join('\n') : '') + '</form>';
+    return htmlTag('form', attrs, content);
 }
 
 function textarea(attrs: HtmlAttributes, content: string[] | null = null): string {
-    return '<textarea ' + attrs.toString() + '>' + (content ? content.join('\n') : '') + '</textarea>';
+    return htmlTag('textarea', attrs, content);
 }
 
 function button(attrs: HtmlAttributes, content: string[] | null = null): string {
-    return '<button ' + attrs.toString() + '>' + (content ? content.join('\n') : '') + '</button>';
+    return htmlTag('button', attrs, content);
 }
 
 function htmlResponse(text: string): Web4Response {
@@ -109,21 +109,49 @@ export function web4_setStaticUrl(url: string): void {
     storage.set(WEB4_STATIC_URL_KEY, url);
 }
 
+function styledPage(html: string): Web4Response {
+    return htmlResponse(`
+        <link rel="stylesheet" href="normalize.css">
+        <link rel="stylesheet" href="skeleton.css">
+        <div class="container">
+            ${html}
+        </div>
+    `);
+}
+
+@nearBindgen
+class GuestBookMessage {
+    sender: string;
+    text: string;
+    premium: boolean;
+}
+
+function messagesHtml(messages: GuestBookMessage[]): string {
+    return `
+        <h1>Guest Book</h1>
+        <table class="u-full-width">
+            <thead>
+                <tr>
+                    <th>Sender</th>
+                    <th>Text</th>
+                    <th>Premium</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${messages.map<string>(message => `
+                    <tr>
+                        <td>${message.sender}</td>
+                        <td>${message.text}</td>
+                        <td>${message.premium ? 'Yes' : 'No'}</td>
+                    </tr>
+                `).join('\n')}
+            </tbody>
+        </table>
+    `;
+}
+
 export function web4_get(request: Web4Request): Web4Response {
     if (request.path == '/test') {
-        if (request.accountId) {
-            // Render HTML with form to submit a message
-            return htmlResponse(form({ action: "/web4/contract/guest-book.testnet/addMessage" }, [
-                `<input type="hidden" name="web4_callback_url" value="${request.path}">`,
-                textarea({ name: "text" }),
-                button({ name: "submit" }, ["Post"])
-            ]));
-        } else {
-            // User needs to login
-            return htmlResponse(`
-                Please <a href="/web4/login?callback_url=${request.path}">login</a> to post messages.
-            `);
-        }
     }
 
     if (request.path == '/messages') {
@@ -133,21 +161,45 @@ export function web4_get(request: Web4Request): Web4Response {
             return preloadUrls([getMessagesUrl]);
         }
 
+        // Parse messages JSON from preloaded response
+        const messages = decode<GuestBookMessage[]>(request.preloads.get(getMessagesUrl).body);
+
+        let formOrLoginLink: string;
+        if (request.accountId) {
+            // Render HTML with form to submit a message
+            formOrLoginLink = form({ action: "/web4/contract/guest-book.testnet/addMessage" }, [
+                `<input type="hidden" name="web4_callback_url" value="${request.path}">`,
+                textarea({ name: "text", className: "u-full-width" }),
+                button({ name: "submit" }, ["Post"])
+            ]);
+        } else {
+            // User needs to login
+            formOrLoginLink = `
+                Please <a href="/web4/login?callback_url=${request.path}">login</a> to post messages.
+            `;
+        }
+
         // Render HTML with messages
-        return htmlResponse('messages: ' + util.bytesToString(request.preloads.get(getMessagesUrl).body)!);
+        return styledPage(messagesHtml(messages) + formOrLoginLink);
+    }
+
+    //  Serve content from IPFS
+    if (request.path == "/" || request.path.endsWith('.css')) {
+        const staticUrl = storage.getString(WEB4_STATIC_URL_KEY);
+        if (staticUrl != null) {
+            return bodyUrl(`${staticUrl!}${request.path}`);
+        }
+
+        // NOTE: ok to return custom response object if needed
+        return { status: 404, contentType: 'text/plain',  body: util.stringToBytes('Static content URL not set. Set using web4_setStaticUrl method.') };
     }
 
     if (request.accountId) {
         // User is logged in, we can welcome them
-        return htmlResponse(`
+        return styledPage(`
             Hello to <b>${request.accountId!}</b> from <code>${request.path}</code>.
             <a href="/web4/logout?callback_url=${request.path}">Logout</a>.
         `);
-    }
-
-    // Demonstrate serving content from IPFS
-    if (request.path == "/" || request.path.endsWith('.css')) {
-        return bodyUrl(`${storage.getString(WEB4_STATIC_URL_KEY)!}${request.path}`);
     }
 
     // By default return 404 Not Found
