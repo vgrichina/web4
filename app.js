b33db32b3fabfa784dd14f7e95d8a7d06973708b
Vladimir Grichina <vgrichina@gmail.com>
2023-02-16 20:09:38 -0800

    Don’t detect body mime type
    IPFS/NEARFS already detect mime type and smart contracts can just return necessary mime type.

--- app.js
+++ app.js
@@ -251,19 +251,6 @@ async function withContractId(ctx, next) {
     return await next();
 }
 
-const mime = require('mime-types');
-function detectMimeType(url, body) {
-    const contentType = mime.lookup(url);
-    if (contentType) {
-        return contentType;
-    }
-    if (body && body.byteLength) {
-        return 'application/octet-stream';
-    }
-    // TODO: Check for other stuff and unify with NEARFS
-    return 'text/plain';
-}
-
 // TODO: Do contract method call according to mapping returned by web4_routes contract method
 // TODO: Use web4_get method in smart contract as catch all if no mapping?
 // TODO: Or is mapping enough?
@@ -326,7 +313,7 @@ router.get('/(.*)', withNear, withContractId, withAccountId, async ctx => {
         }
 
         if (body) {
-            ctx.type = contentType || detectMimeType(bodyUrl, body);
+            ctx.type = contentType
             ctx.body = Buffer.from(body, 'base64');
             return;
         }
