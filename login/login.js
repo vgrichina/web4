57c5a8570e2fbdea290d3eda90c3f52c7dbff83b
Vladimir Grichina <vgrichina@gmail.com>
2023-11-01 18:26:02 -0700

    Add Meteor Wallet sign in

--- login/login.js
+++ login/login.js
@@ -1,6 +1,8 @@
 import Cookies from 'js-cookie';
 import { HereWallet } from "@here-wallet/core";
-import { KeyPair } from "near-api-js";
+import { MeteorWallet } from "@meteorwallet/sdk";
+import { connect, KeyPair } from "near-api-js";
+import { BrowserLocalStorageKeyStore } from 'near-api-js/lib/key_stores';
 
 import { signInURL } from "../util/web-wallet-api";
 
@@ -32,5 +34,41 @@ async function signInNEARWallet({
     });
 }
 
+async function signInMeteorWallet({ contractId }) {
+    const keyPair = KeyPair.fromRandom('ed25519');
+    Cookies.set('web4_account_id');
+    Cookies.set('web4_private_key', keyPair.toString());
+
+    const keyStore = new BrowserLocalStorageKeyStore(window.localStorage, "_meteor_wallet");
+
+    const near = await connect({
+        keyStore,
+        // TODO: Propagate this from config
+        networkId: "mainnet",
+        // headers: {},
+    });
+
+    const wallet = new MeteorWallet({ near, appKeyPrefix: "near_app" });
+
+    const { success, payload: { accountId }} = await wallet.requestSignIn({
+        contract_id: contractId,
+        type: "ALL_METHODS",
+        keyPair
+    });
+
+    if (!success) {
+        console.log('Meteor Wallet sign in failed');
+        return;
+        // TODO: Check how to handle failure
+    }
+
+    Cookies.set('web4_account_id', accountId);
+    Cookies.set('web4_private_key', keyPair.toString());
+
+    // NOTE: CALLBACK_URL set in login.html
+    window.location.href = CALLBACK_URL;
+}
+
 window.signInHereWallet = signInHereWallet;
 window.signInNEARWallet = signInNEARWallet;
+window.signInMeteorWallet = signInMeteorWallet;
